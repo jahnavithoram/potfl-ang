66ee98843409eb0ce080fd63c454057fad82eac6
jahnavithoram <41298477+jahnavithoram@users.noreply.github.com>
2021-03-31 00:19:59 +0530

    Add files via upload

---
+++ src/app/blogin/blogin.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BloginComponent } from './blogin.component';
+
+describe('BloginComponent', () => {
+  let component: BloginComponent;
+  let fixture: ComponentFixture<BloginComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BloginComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BloginComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
